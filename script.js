049f66eb8ec34c75452cd7ecc33f2aa89dc010b4
normanfmailer <132857148+normanfmailer@users.noreply.github.com>
2024-11-29 00:04:58 -0500

    Create script.js
    Added the original javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+if(window.location.href.includes('lpsg.com')) {
+    deleteMe = Array.from(document.getElementsByClassName("video-easter-egg-overlay"));
+    // deleteMe.concat(document.getElementsByClassName("video-easter-egg-blocker"));
+    for(let i = 0; i < deleteMe.length; i++) {
+        deleteMe[i].remove();
+    }
+
+    turnToLinks = document.getElementsByClassName("video-easter-egg-poster");
+    for(let i = 0; i < turnToLinks.length; i++) {
+        turnToLinks[i].children[0].style.cursor = "pointer";
+        turnToLinks[i].children[0].addEventListener('click', (event) => {
+            var img = event.target || event.srcElement;
+            window.open(img.src.replace("attachments/posters", "video").replace(".jpg", ".mp4").replace("thumbnail", "video"));
+        });
+}}
